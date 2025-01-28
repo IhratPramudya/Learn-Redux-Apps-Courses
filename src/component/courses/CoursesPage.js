dafba6ae21e411d82a7ee4b7ae75790e216ac0b5
ihrat pramudya <ihratpramudya>
2025-01-28 23:45:43 +0700

    Add redux Course Actions

--- src/component/courses/CoursesPage.js
+++ src/component/courses/CoursesPage.js
@@ -1,18 +1,29 @@
 import React from "react";
 
 class CoursePage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            course: {
-                title: ""
-            }
+    state = {
+        course: {
+            title: ""
         }
     }
+
+    handleChange = event => {
+        const course = {...this.state.course, title: event.target.value};
+        this.setState({ course: course });
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        alert(this.state.course.title);
+    }
+
     render() {
         return (
-            <form>
-                
+            <form onSubmit={this.handleSubmit} >
+                <h2>Courses</h2>
+                <h3>Add Course</h3>
+                <input type="text" onChange={this.handleChange} value={this.state.course.title} />
+                <input type="submit" value="save" />
             </form>
         )
     }
