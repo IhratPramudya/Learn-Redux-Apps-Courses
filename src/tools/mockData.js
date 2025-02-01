efee1b83282aee3e0574c87086f012be88d50e9f
ihrat pramudya <ihratpramudya>
2025-02-01 18:00:02 +0700

    Add my Redux async and thunk

---
+++ src/tools/mockData.js
@@ -0,0 +1,85 @@
+const courses = [
+    {
+        id: 1,
+        title: "Security React Apps with Auth0",
+        slug: "react-auth0-authentication-security",
+        authorId: 1,
+        category: "JavaScript"
+    },
+    {
+        id: 2,
+        title: "Security React Apps with Auth0",
+        slug: "react-auth0-authentication-security",
+        authorId: 1,
+        category: "JavaScript"
+    },
+    {
+        id: 3,
+        title: "Security React Apps with Auth0",
+        slug: "react-auth0-authentication-security",
+        authorId: 1,
+        category: "JavaScript"
+    },
+    {
+        id: 4,
+        title: "Security React Apps with Auth0",
+        slug: "react-auth0-authentication-security",
+        authorId: 1,
+        category: "JavaScript"
+    },
+    {
+        id: 5,
+        title: "Security React Apps with Auth0",
+        slug: "react-auth0-authentication-security",
+        authorId: 1,
+        category: "JavaScript"
+    },
+    {
+        id: 6,
+        title: "Security React Apps with Auth0",
+        slug: "react-auth0-authentication-security",
+        authorId: 1,
+        category: "JavaScript"
+    },
+    {
+        id: 7,
+        title: "Security React Apps with Auth0",
+        slug: "react-auth0-authentication-security",
+        authorId: 1,
+        category: "JavaScript"
+    },
+    {
+        id: 8,
+        title: "Security React Apps with Auth0",
+        slug: "react-auth0-authentication-security",
+        authorId: 1,
+        category: "JavaScript"
+    },
+    {
+        id: 9,
+        title: "Security React Apps with Auth0",
+        slug: "react-auth0-authentication-security",
+        authorId: 1,
+        category: "JavaScript"
+    },
+]
+
+const authors = [
+    {id: 1, name: "Cory House"},
+    {id: 2, name: "Scott Allen"},
+    {id: 3, name: "Dan Wahlin"}
+]
+
+const newCourse = {
+    id: null,
+    title: "",
+    authorId: null,
+    category: ""
+}
+
+
+module.exports = {
+    newCourse,
+    courses,
+    authors
+}
